f7fbaff33b93a845f638ed0d39865fa8c136c297
Gisele Bezerra de Almeida <110948857+GiseleBezerra@users.noreply.github.com>
2023-09-14 13:43:58 -0300

    Create 1007.js

---
+++ JavaScript/1007.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+a = parseInt(lines.shift())
+b = parseInt(lines.shift())
+c = parseInt(lines.shift())
+d = parseInt(lines.shift())
+diferenca = (a * b - c * d)
+
+console.log(`DIFERENCA = ${diferenca}`)
